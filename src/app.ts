40a1a7ea2cc14c20a07bd3aebcf1a8ffc599f1b1
aghyad-fanous <aghyad.fanous.work@gmail.com>
2025-11-09 15:12:28 +0300

    Allowed Origins

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app: Application = express();
 
 // السماح فقط للفرونت المحلي ونسخة Vercel
 const allowedOrigins = [
-  "http://localhost:3000",
+  "localhost:3000",
   "https://aghyad-fanous-portfolio.vercel.app"
 ]
 
